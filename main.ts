5d251089a5ac4f1b53437354029cd8a33d7fc8ec
j-vanwijk <60421659+j-vanwijk@users.noreply.github.com>
2021-11-28 10:04:14 +0100

    klaar

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+let sensorRechts = 0
+let sensorLinks = 0
 basic.forever(function () {
-	
+    let verschilSenoren = 0
+    sensorRechts = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+    sensorLinks = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+    sensorLinks = sensorLinks - sensorRechts
+    if (verschilSenoren > 10) {
+        if (sensorLinks > sensorRechts) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # . # # #
+                . # . . .
+                . . # . .
+                `)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # . #
+                . . . # .
+                . . # . .
+                `)
+        }
+    } else {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
+    }
 })
